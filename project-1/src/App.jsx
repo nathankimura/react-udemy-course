67059ca3eec408d62da88d43d749c0091b1a3a04
nathan <nathankimura14@gmail.com>
2023-03-16 18:28:15 -0300

    fix: just a commit to fix the background color, that wasn't appearing outside the class container

--- project-1/src/App.jsx
+++ project-1/src/App.jsx
@@ -3,42 +3,42 @@ import { Component } from 'react'
 
 class App extends Component {
   state = {
-      posts: [],
-    };
+    posts: [],
+  };
 
-    componentDidMount() {
-      this.loadPosts();
-   }
+  componentDidMount() {
+    this.loadPosts();
+  }
 
-    loadPosts = async () => {
-      const postsResponse = fetch('https://jsonplaceholder.typicode.com/posts');
+  loadPosts = async () => {
+    const postsResponse = fetch('https://jsonplaceholder.typicode.com/posts');
 
-      const photosResponse = fetch('https://jsonplaceholder.typicode.com/photos');
+    const photosResponse = fetch('https://jsonplaceholder.typicode.com/photos');
 
-      const [posts, photos] = await Promise.all([postsResponse, photosResponse]);
+    const [posts, photos] = await Promise.all([postsResponse, photosResponse]);
 
-      const postsJson = await posts.json();
-      const photosJson = await photos.json();
+    const postsJson = await posts.json();
+    const photosJson = await photos.json();
 
-      const postsAndPhotos = postsJson.map((post, index) => {
-        return { ...post, cover: photosJson[index].url }
-      });
+    const postsAndPhotos = postsJson.map((post, index) => {
+      return { ...post, cover: photosJson[index].url }
+    });
 
-      this.setState({ posts: postsAndPhotos });
-    }
-    
+    this.setState({ posts: postsAndPhotos });
+  }
+
   render() {
-    const {posts} = this.state
+    const { posts } = this.state
 
     return (
       <section className='container'>
         <div className="posts">
           {posts.map((post) => (
             <div className="post">
-              <img src={post.cover} alt={post.title}/>
-              <div 
+              <img src={post.cover} alt={post.title} />
+              <div
                 key={post.id}
-                className = "post-content"
+                className="post-content"
               >
                 <h1>{post.title}</h1>
                 <p>{post.body}</p>
