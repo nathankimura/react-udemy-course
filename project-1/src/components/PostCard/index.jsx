e2ff22165d52a9de166d90263ee2a509558b545e
nathan <nathankimura14@gmail.com>
2023-03-17 08:00:47 -0300

    feat: starting componentization

---
+++ project-1/src/components/PostCard/index.jsx
@@ -0,0 +1,12 @@
+export const PostCard = ({ title, body, cover, id }) => (
+  <div className="post">
+    <img src={cover} alt={title} />
+    <div
+      key={id}
+      className="post-content"
+    >
+      <h1>{title}</h1>
+      <p>{body}</p>
+    </div>
+  </div>
+);
