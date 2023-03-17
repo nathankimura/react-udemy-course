26008a61b4248b4350f156ff46036719b89fa4d8
nathan <nathankimura14@gmail.com>
2023-03-17 08:28:23 -0300

    feat: creating more components, and organizing page

--- project-1/src/App.jsx
+++ project-1/src/App.jsx
@@ -1,30 +1,20 @@
 import './App.css'
 import { Component } from 'react'
 import { PostCard } from './components/PostCard';
+import { loadPosts } from './utils/load-posts';
+import { Posts } from './components/Posts';
 
 class App extends Component {
   state = {
     posts: [],
   };
 
-  componentDidMount() {
-    this.loadPosts();
+  async componentDidMount() {
+    await this.loadPosts();
   }
 
   loadPosts = async () => {
-    const postsResponse = fetch('https://jsonplaceholder.typicode.com/posts');
-
-    const photosResponse = fetch('https://jsonplaceholder.typicode.com/photos');
-
-    const [posts, photos] = await Promise.all([postsResponse, photosResponse]);
-
-    const postsJson = await posts.json();
-    const photosJson = await photos.json();
-
-    const postsAndPhotos = postsJson.map((post, index) => {
-      return { ...post, cover: photosJson[index].url }
-    });
-
+    const postsAndPhotos = await loadPosts();
     this.setState({ posts: postsAndPhotos });
   }
 
@@ -33,17 +23,9 @@ class App extends Component {
 
     return (
       <section className='container'>
-        <div className="posts">
-          {posts.map((post) => (
-            <PostCard
-              key={post.id}
-              title={post.title}
-              body={post.body}
-              cover={post.cover}
-              id={post.id}
-            />
-          ))}
-        </div>
+        <Posts
+          posts={posts}
+        />
       </section>
     )
   }
--- project-1/src/components/PostCard/index.jsx
+++ project-1/src/components/PostCard/index.jsx
@@ -2,10 +2,9 @@ export const PostCard = ({ title, body, cover, id }) => (
   <div className="post">
     <img src={cover} alt={title} />
     <div
-      key={id}
       className="post-content"
     >
-      <h1>{title}</h1>
+      <h2>{title}</h2>
       <p>{body}</p>
     </div>
   </div>
